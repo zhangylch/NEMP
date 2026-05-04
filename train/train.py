13e9a97dc1c032009c111444282103e776029cd2
zhangyaolong <56628666+zhangylch@users.noreply.github.com>
2026-05-03 21:11:39 -0600

    Update training config for cueq tensor product metadata

--- train/train.py
+++ train/train.py
@@ -136,7 +136,7 @@ def train(params, ema_params, config, optim, opt_state, lr_state, schedule_fn, v
                 opt_state=ave_opt_state, 
                 config=config, 
                 ckpt_dir=full_config.ckpath,
-                max_to_keep=5  # <--- 只保留最近 5 个，自动删除更早的
+                max_to_keep=5  # keep the latest checkpoints
             )
 
             print(f"Step {start_step}: Saved checkpoint")
@@ -187,7 +187,7 @@ coor, cell, neighlist, celllist, shiftimage, center_factor, species, numatoms, a
 initdata = (coor[0], cell[0], jnp.zeros_like(cell[0]), neighlist[0], celllist[0], shiftimage[0], center_factor[0], species[0])
 
 #=================================================Equi MPNN===================================================================
-config = ModelConfig(nspec=nspec, num_cg=num_cg, emb_nl=full_config.emb_nl, MP_nl=full_config.MP_nl, radial_nl=full_config.radial_nl, out_nl=full_config.out_nl, reduce_spec=reduce_spec, com_spec=com_spec, count_l=count_l, index_l=index_l, index_i1=index_i1, index_i2=index_i2, ens_cg=ens_cg, index_add=index_add, index_den=index_den, index_squ=index_squ, initbias_neigh=initbias_neigh, cutoff=full_config.cutoff, npaircode=full_config.npaircode, nradial=full_config.nradial, nwave=full_config.nwave, rmaxl=rmaxl, prmaxl=prmaxl, MP_loop=full_config.MP_loop, pn=full_config.pn, use_norm=full_config.use_norm, use_bias=full_config.use_bias, std=force_std, cst=1.67462)
+config = ModelConfig(nspec=nspec, num_cg=num_cg, emb_nl=full_config.emb_nl, MP_nl=full_config.MP_nl, radial_nl=full_config.radial_nl, out_nl=full_config.out_nl, reduce_spec=reduce_spec, com_spec=com_spec, count_l=count_l, index_l=index_l, ens_cg=ens_cg, initbias_neigh=initbias_neigh, cutoff=full_config.cutoff, npaircode=full_config.npaircode, nradial=full_config.nradial, nwave=full_config.nwave, rmaxl=rmaxl, prmaxl=prmaxl, MP_loop=full_config.MP_loop, pn=full_config.pn, use_norm=full_config.use_norm, use_bias=full_config.use_bias, std=force_std, cst=1.67462)
 
 model = MPNN.MPNN(config)
 
