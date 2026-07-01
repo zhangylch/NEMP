d25ce52123a4c69f60901aa06d951f78004f0e29
zhangylch <56628666+zhangylch@users.noreply.github.com>
2026-07-01 16:41:18 +0800

    Remove apply_if_finite optimizer wrapper

--- train/train.py
+++ train/train.py
@@ -281,14 +281,10 @@ value_fn = make_loss(pes_model, nprop)
 
 schedule_fn = optax.contrib.reduce_on_plateau(factor=full_config.decay_factor, patience=full_config.patience_step, cooldown=full_config.cooldown, min_scale=full_config.elr/full_config.slr)
 
-#optim = optax.amsgrad(learning_rate=slr)
-optim = optax.apply_if_finite(
-    optax.chain(
-        optax.add_decayed_weights(full_config.weight_decay),
-        optax.clip_by_global_norm(full_config.clip_norm),
-        optax.amsgrad(learning_rate=full_config.slr),
-    ),
-    max_consecutive_errors=5,
+optim = optax.chain(
+    optax.add_decayed_weights(full_config.weight_decay),
+    optax.clip_by_global_norm(full_config.clip_norm),
+    optax.amsgrad(learning_rate=full_config.slr),
 )
 
 opt_state = optim.init(params)
